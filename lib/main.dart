b76c867874343f71ee9dcfcb426915b26a5343fe
Zeeshaan Maudarbocus <zeeshaan@touchfoundry.co.za>
2020-09-16 17:24:53 +0200

    use new package to format date

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -84,7 +85,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          transaction.date.toString(),
+                          DateFormat.yMMMd().format(transaction.date),
                           style: TextStyle(
                             color: Colors.grey,
                           ),
